015d09ae2eddfb5f6817e87ee8ecd2cdf530d328
DMBarber <dennis@dmbwebdesigns.com>
2018-04-05 22:26:14 -0400

    Updated to allow for css modules. This may not be available if the config is not updated as well for this setting.

--- burgerbuilder/src/Person/Person.js
+++ burgerbuilder/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
     return (
-            <div className="Person">
-                <button className="btn close" onClick={props.click}>&times;</button>
+            <div className={classes.Person}>
+                <button className={classes.Closebutton} onClick={props.click}>&times;</button>
                 <p>I'm {props.name} and I am {props.age} years old!</p>
                 <p>{props.children}</p>
                         <input type="text" onChange={props.changed} value={props.name} />
